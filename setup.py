eaade39016f039ef00127a9cc9e96b08a3771ba1
= <=>
2023-06-10 23:40:32 +0100

    updates to version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="carter-py",
-    version="0.2.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires = [
     "aiohttp==3.8.4",
